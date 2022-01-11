a037d88b32f5f23367550464b5a0d05d1965fd3f
Oscar <oscar.andres.vargas1@gmail.com>
2022-01-11 11:36:33 +0100

    Server setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,26 @@
-console.log('this is from app');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import config from 'config';
+
+const port = config.get<number>('port');
+const host = config.get<string>('host');
+const corsOrigin = config.get<string>('corsOrigin');
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get('/', (_, res) => res.send('Server is up'));
+
+httpServer.listen(port, host, () => {
+  console.log(`Server is listening on port ${port}`);
+});
